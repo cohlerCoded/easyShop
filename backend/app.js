91b80694df284d7873c65af07b9c622d4de9cfe7
cohlerCoded <jeffcohler@cohlercoded.com>
2021-10-21 22:39:12 -0700

    reading environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
+
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.get(api + '/products', (req, res) => {
   res.send('Welcome to Eshop')
 })
+
 app.listen(3000, () => {
+  console.log(api)
   console.log('Server listening on port 3000')
 })
