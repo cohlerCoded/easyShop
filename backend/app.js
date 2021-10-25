d5b089b52e582ec956162424fb7a8faf2843f1c0
cohlerCoded <jeffcohler@cohlercoded.com>
2021-10-25 14:48:21 -0700

    something off

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const morgan = require('morgan')
 const Product = mongoose.model('Product')
-
+const productRouter = require('./routers/productRoutes')
 require('dotenv/config')
 
 const api = process.env.API_URL
@@ -13,6 +13,7 @@ const mongoUrl = process.env.MONGODB_URL
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(`${api}/products`, productRouter)
 
 mongoose
   .connect(mongoUrl, {
@@ -25,30 +26,4 @@ mongoose
 app.listen(3000, () => {
   console.log(api)
   console.log('Server listening on port 3000')
-})
-
-app.get(`${api}/products`, async (req, res) => {
-  try {
-    const productList = await Product.find()
-    res.send(productList)
-  } catch (error) {
-    res.status(500).json({ success: false })
-  }
-})
-app.post(`${api}/products`, async (req, res) => {
-  try {
-    const { name, image, countInStock } = req.body
-    const product = new Product({
-      name,
-      image,
-      countInStock,
-    })
-    const savedProduct = await product.save()
-    res.status(201).json(savedProduct)
-  } catch (error) {
-    res.status(500).json({
-      error,
-      success: false,
-    })
-  }
 })
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+router.get(`${api}/products`, async (req, res) => {
+  try {
+    const productList = await Product.find()
+    res.send(productList)
+  } catch (error) {
+    res.status(500).json({ success: false })
+  }
+})
+
+router.post(`${api}/products`, async (req, res) => {
+  try {
+    const { name, image, countInStock } = req.body
+    const product = new Product({
+      name,
+      image,
+      countInStock,
+    })
+    const savedProduct = await product.save()
+    res.status(201).json(savedProduct)
+  } catch (error) {
+    res.status(500).json({
+      error,
+      success: false,
+    })
+  }
+})
+
+module.exports = router
