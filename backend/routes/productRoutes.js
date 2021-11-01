95b371a9d1a73a738937573100466a7c60ffc664
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-01 14:19:01 -0700

    post a new product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Category } = require('../models/Category')
 const router = express.Router()
 const { Product } = require('../models/Product')
 
@@ -13,17 +14,43 @@ router.get(`/`, async (req, res) => {
 
 router.post(`/`, async (req, res) => {
   try {
-    const { name, image, countInStock } = req.body
+    const {
+      name,
+      description,
+      richDescription,
+      image,
+      images,
+      brand,
+      price,
+      countInStock,
+      rating,
+      numReviews,
+      isFeatured,
+    } = req.body
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid Category')
+
     const product = new Product({
       name,
+      description,
+      richDescription,
       image,
+      images,
+      brand,
+      price,
+      category,
       countInStock,
+      rating,
+      numReviews,
+      isFeatured,
     })
     const savedProduct = await product.save()
-    res.status(201).json(savedProduct)
+    if (!savedProduct) return res.status(500).send('Product can not be created')
+    res.status(201).send(savedProduct)
   } catch (error) {
+    console.log(error)
     res.status(500).json({
-      error,
+      message: error.message,
       success: false,
     })
   }
