b8e1a4b31fe2cec8b21e1ac50a43a37f3538666f
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-02 14:39:38 -0700

    update a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,9 +5,7 @@ const { Product } = require('../models/Product')
 
 router.get(`/`, async (req, res) => {
   try {
-    const productList = await Product.find()
-      .select('name image category -_id')
-      .populate('category')
+    const productList = await Product.find().populate('category')
     res.send(productList)
   } catch (error) {
     res.status(500).json({ success: false })
@@ -30,35 +28,22 @@ router.get(`/:id`, async (req, res) => {
 
 router.post(`/`, async (req, res) => {
   try {
-    const {
-      name,
-      description,
-      richDescription,
-      image,
-      images,
-      brand,
-      price,
-      countInStock,
-      rating,
-      numReviews,
-      isFeatured,
-    } = req.body
     const category = await Category.findById(req.body.category)
     if (!category) return res.status(400).send('Invalid Category')
 
     const product = new Product({
-      name,
-      description,
-      richDescription,
-      image,
-      images,
-      brand,
-      price,
-      category,
-      countInStock,
-      rating,
-      numReviews,
-      isFeatured,
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      images: req.body.images,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
     })
     const savedProduct = await product.save()
     if (!savedProduct) return res.status(500).send('Product can not be created')
@@ -69,6 +54,42 @@ router.post(`/`, async (req, res) => {
       message: error.message,
       success: false,
     })
+  }
+})
+
+//UPDATE A PRODUCT
+
+router.put(`/:id`, async (req, res) => {
+  try {
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid Category')
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+      },
+      { new: true }
+    )
+    if (!updatedProduct) {
+      res
+        .status(404)
+        .send({ message: 'Product with given Id not found', success: false })
+    }
+    res.status(200).send(updatedProduct)
+  } catch (error) {
+    res.status(500).json({ success: false })
   }
 })
 
