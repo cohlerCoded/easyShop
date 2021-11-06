8f6dfc2155f5aca99714fe46bca8d1d11700cf1d
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-06 12:16:49 -0700

    User Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,59 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  apartment: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  city: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  zip: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  country: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+})
+
+productSchema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+productSchema.set('toJSON', {
+  virtuals: true,
 })
 
 exports.User = mongoose.model('User', userSchema)
