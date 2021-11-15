7250a31ddec02413fc64dce8afd5f175be6b4a70
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-15 11:03:55 -0800

    excluding routes from requiring token

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,14 +1,16 @@
 const expressJwt = require('express-jwt')
 
 function authJwt() {
+  const api = process.env.API_URL
   return expressJwt({
     secret: process.env.JWT_SECRET,
     algorithms: ['HS256'],
   }).unless({
     path: [
-      { url: '/api/v1/products', methods: ['GET', 'OPTIONS'] },
-      '/api/v1/users/login',
-      '/api/v1/users/register',
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      `${api}/v1/users/login`,
+      `${api}/v1/users/register`,
     ],
   })
 }
