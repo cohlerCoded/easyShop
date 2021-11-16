b4dc28cd0e210726b1904d52dc5b2ea53a621d5f
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-16 13:34:31 -0800

    POST a new order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,10 +2,36 @@ const express = require('express')
 const router = express.Router()
 const { Order } = require('../models/Order')
 
+// GET ALL ORDERS
+
 router.get(`/`, async (req, res) => {
   try {
     const orderList = await Order.find()
     res.send(orderList)
+  } catch (error) {
+    res.status(500).json({ success: false })
+  }
+})
+
+// POST A NEW ORDER
+
+router.post('/', async (req, res) => {
+  try {
+    let order = new Order({
+      orderItems: req.body.orderItems,
+      user: req.body.user,
+      address1: req.body.address1,
+      address2: req.body.address2,
+      city: req.body.city,
+      zip: req.body.zip,
+      country: req.body.country,
+      phone: req.body.phone,
+      status: req.body.status,
+      toatlPrice: req.body.toatlPrice,
+    })
+    order = await category.save()
+    !order && res.status(400).send('Order could not be created')
+    res.send(order)
   } catch (error) {
     res.status(500).json({ success: false })
   }
