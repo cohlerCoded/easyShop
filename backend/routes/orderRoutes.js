25341dc4db3d6eb8c4d9c80b64c1a5ce1bf2c903
cohlerCoded <jeffcohler@cohlercoded.com>
2021-11-19 10:43:04 -0800

    remove orderitems when deleting order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -95,8 +95,14 @@ router.put(`/:id`, async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const id = await Order.findById(req.params.id)
-    if (id) res.status(200).json({ success: true, message: 'Deleted!' })
-    else res.status(404).json({ success: false, message: 'Order not found' })
+    if (id) {
+      req.body.orderItems.forEach(
+        async (itemId) => await findByIdAndRemove(itemId)
+      )
+      res.status(200).json({ success: true, message: 'Deleted!' })
+    } else {
+      res.status(404).json({ success: false, message: 'Order not found' })
+    }
     await Order.findByIdAndRemove(id)
   } catch (error) {
     res.status(400).json({ success: false, message: error.message })
