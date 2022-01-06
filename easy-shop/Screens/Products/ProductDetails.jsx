936b843cb60dd6e9eef865f046276c06d53001c6
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-06 15:07:40 -0800

    product details styling

--- easy-shop/Screens/Products/ProductDetails.jsx
+++ easy-shop/Screens/Products/ProductDetails.jsx
@@ -14,21 +14,38 @@ const { width } = Dimensions.get('window')
 
 const ProductDetails = (props) => {
   const { item } = props.route.params
-  console.log(item)
   return (
-    <ScrollView style={styles.scrollView}>
-      <Image
-        style={styles.image}
-        resizeMode='contain'
-        source={
-          item.image
-            ? { uri: item.image }
-            : {
-                uri: 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
-              }
-        }
-      />
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Image
+          style={styles.image}
+          resizeMode='contain'
+          source={
+            item.image
+              ? { uri: item.image }
+              : {
+                  uri: 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
+                }
+          }
+        />
+        <View style={styles.contentContainer}>
+          <Heading size='xl' style={{ marginBottom: 20 }}>
+            {item.name}
+          </Heading>
+          <Heading size='md'>{item.brand}</Heading>
+        </View>
+        {/*   todo rich desc availability */}
+      </ScrollView>
+      <View style={styles.bottomContainer}>
+        <VStack w='100%'>
+          <HStack justifyContent='space-between'>
+            <Text style={styles.price}>${item.price}</Text>
+
+            <Button title='Add' />
+          </HStack>
+        </VStack>
+      </View>
+    </View>
   )
 }
 
@@ -51,5 +68,28 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 0,
     margin: 0,
+  },
+  contentContainer: {
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    alignContent: 'space-between',
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'white',
+  },
+  price: {
+    fontSize: 24,
+    margin: 20,
+    color: 'red',
   },
 })
