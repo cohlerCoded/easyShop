7c4c4ecefe085110c8736056eaf62bdf4a5686cd
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-05 22:10:21 -0800

    details image

--- easy-shop/Screens/Products/ProductDetails.jsx
+++ easy-shop/Screens/Products/ProductDetails.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Button,
+  Dimensions,
+} from 'react-native'
+import { Heading, VStack, HStack, Container, Icon } from 'native-base'
+
+const { width } = Dimensions.get('window')
 
 const ProductDetails = (props) => {
   const { item } = props.route.params
+  console.log(item)
   return (
-    <View>
-      <Text>{item.name}</Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <Image
+        style={styles.image}
+        resizeMode='contain'
+        source={
+          item.image
+            ? { uri: item.image }
+            : {
+                uri: 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
+              }
+        }
+      />
+    </ScrollView>
   )
 }
 
 export default ProductDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    height: '100%',
+  },
+  scrollView: {
+    marginBottom: 80,
+    padding: 5,
+  },
+  image: {
+    width: '100%',
+    height: 250,
+  },
+  imageContainer: {
+    backgroundColor: 'white',
+    padding: 0,
+    margin: 0,
+  },
+})
