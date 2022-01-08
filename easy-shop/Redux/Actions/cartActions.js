1ab217805ded1050f3a9a563c96ba85e72eeb06b
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-08 13:02:34 -0800

    cart actions

---
+++ easy-shop/Redux/Actions/cartActions.js
@@ -0,0 +1,11 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants'
+
+const addToCart = (payload) => {
+  return { type: ADD_TO_CART, payload }
+}
+const removeFromCart = (payload) => {
+  return { type: REMOVE_FROM_CART, payload }
+}
+const clearCart = () => {
+  return { type: CLEAR_CART }
+}
