73600c6f5eadca0079984908344ddaf96ebd2683
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-11 23:33:47 -0800

    idea for add by qty

--- easy-shop/Redux/Actions/cartActions.js
+++ easy-shop/Redux/Actions/cartActions.js
@@ -1,7 +1,18 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants'
 
-export const addToCart = (payload) => {
-  return { type: ADD_TO_CART, payload }
+export const addToCart = (payload, qty) => {
+  return {
+    type: ADD_TO_CART,
+    payload: {
+      ...payload,
+      qtyInCart:
+        qty && payload.qtyInCart
+          ? (payload.qtyInCart += qty)
+          : payload.qtyInCart > 0
+          ? payload.qtyInCart
+          : 0,
+    },
+  }
 }
 export const removeFromCart = (payload) => {
   return { type: REMOVE_FROM_CART, payload }
