9556fedceded93ecbf70562ab4308373735aba73
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-18 14:09:17 -0800

    card styling

--- easy-shop/Screens/Products/ProductCard.jsx
+++ easy-shop/Screens/Products/ProductCard.jsx
@@ -52,11 +52,11 @@ export default ProductCard
 const styles = StyleSheet.create({
   container: {
     width: width / 2 - 20,
-    height: width / 1.7,
+    height: width / 1.15,
     padding: 10,
     borderRadius: 10,
-    marginTop: 55,
-    marginBottom: 5,
+    marginTop: 20,
+    marginBottom: 20,
     marginLeft: 10,
     alignItems: 'center',
     elevation: 8,
@@ -66,14 +66,10 @@ const styles = StyleSheet.create({
     width: width / 2 - 20 - 10,
     height: width / 2 - 20 - 30,
     backgroundColor: 'transparent',
-    position: 'absolute',
-    top: -45,
+    marginTop: 20,
   },
   card: {
     marginBottom: 10,
-    height: width / 2 - 20 - 90,
-    backgroundColor: 'transparent',
-    width: width / 2 - 20 - 10,
   },
   title: {
     fontWeight: 'bold',
@@ -83,6 +79,6 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 20,
     color: 'orange',
-    marginTop: 10,
+    margin: 10,
   },
 })
