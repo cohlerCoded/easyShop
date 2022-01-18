3cd4ef76ca4c26c7196518d146e2596b4b82ee6e
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-18 13:38:08 -0800

    change qty in cart action

--- easy-shop/Redux/Actions/cartActions.js
+++ easy-shop/Redux/Actions/cartActions.js
@@ -1,4 +1,9 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants'
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  CHANGE_QTY_IN_CART,
+} from '../constants'
 
 export const addToCart = (payload, qty) => (dispatch, getState) => {
   const cart = getState().cartItems
@@ -18,6 +23,18 @@ export const addToCart = (payload, qty) => (dispatch, getState) => {
       qtyInCart: qty,
     },
   })
+}
+
+export const changeQtyInCart = (payload, qty) => (dispatch, getState) => {
+  const cart = getState().cartItems
+  for (const item of cart) {
+    if (item._id === payload._id) {
+      return dispatch({
+        type: CHANGE_QTY_IN_CART,
+        payload: { id: payload._id, qty },
+      })
+    }
+  }
 }
 
 export const removeFromCart = (payload) => {
