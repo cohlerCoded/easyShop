b52a5cb2159883739a34928b7c873754ddf59fdf
cohlerCoded <jeffcohler@cohlercoded.com>
2022-01-30 13:19:47 -0800

    some clean up/ flatlist cart

--- easy-shop/Screens/Cart/CartItem.jsx
+++ easy-shop/Screens/Cart/CartItem.jsx
@@ -2,47 +2,27 @@ import React, { useEffect, useState } from 'react'
 import {
   StyleSheet,
   Text,
-  View,
-  Button,
   TouchableOpacity,
   Dimensions,
   TextInput,
 } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import {
-  addToCart,
   changeQtyInCart,
-  clearCart,
   removeFromCart,
 } from '../../Redux/Actions/cartActions'
 
 import { FontAwesome } from 'react-native-vector-icons'
-import {
-  Image,
-  Container,
-  Heading,
-  HStack,
-  VStack,
-  Divider,
-  Icon,
-  ScrollView,
-} from 'native-base'
-import EasyButton from '../../Components/EasyButton'
+import { Image, HStack, VStack, Divider, Icon } from 'native-base'
 
 const { height, width } = Dimensions.get('window')
 
 const CartItem = ({ navigation, item }) => {
   const dispatch = useDispatch()
-  let cartItems = useSelector((state) => state.cartItems)
-  console.log(cartItems)
+
   //Calculate Prices
   const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2)
-  let totalPrice = addDecimals(
-    cartItems.reduce(
-      (sumPrice, item) => sumPrice + item.price * item.qtyInCart,
-      0
-    )
-  )
+
   return (
     <VStack space={3}>
       <TouchableOpacity
