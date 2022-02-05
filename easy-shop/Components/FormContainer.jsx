0e323669c37fb8a63c88bdce89e1243d5928c28e
cohlerCoded <jeffcohler@cohlercoded.com>
2022-02-05 14:22:41 -0800

    FormContainer

---
+++ easy-shop/Components/FormContainer.jsx
@@ -0,0 +1,28 @@
+import { ScrollView, Dimensions, StyleSheet, Text } from 'react-native'
+import React from 'react'
+
+const { width } = Dimensions.get('window')
+
+const FormContainer = (props) => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>{props.title}</Text>
+      {props.children}
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    marginBottom: 400,
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+  },
+})
+
+export default FormContainer
