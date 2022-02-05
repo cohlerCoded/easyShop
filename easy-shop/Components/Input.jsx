d0071d2f923db4007ae8c530f7a70749bc175c93
cohlerCoded <jeffcohler@cohlercoded.com>
2022-02-05 14:39:40 -0800

    input label

--- easy-shop/Components/Input.jsx
+++ easy-shop/Components/Input.jsx
@@ -3,22 +3,28 @@ import { TextInput, StyleSheet } from 'react-native'
 
 const Input = (props) => {
   return (
-    <TextInput
-      style={styles.input}
-      placeholder={props.placeholder}
-      name={props.name}
-      id={props.id}
-      value={props.value}
-      autoCorrect={props.autoCorrect}
-      onChangeText={props.onChangeText}
-      onFocus={props.onFocus}
-      secureTextEntry={props.secureTextEntry}
-      keyboardType={props.keyboardType}
-    ></TextInput>
+    <>
+      {props.label && <Text style={style.label}>{props.label}</Text>}
+      <TextInput
+        style={styles.input}
+        placeholder={props.placeholder}
+        name={props.name}
+        id={props.id}
+        value={props.value}
+        autoCorrect={props.autoCorrect}
+        onChangeText={props.onChangeText}
+        onFocus={props.onFocus}
+        secureTextEntry={props.secureTextEntry}
+        keyboardType={props.keyboardType}
+      ></TextInput>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
+  label: {
+    fontSize: 16,
+  },
   input: {
     width: '80%',
     height: 60,
