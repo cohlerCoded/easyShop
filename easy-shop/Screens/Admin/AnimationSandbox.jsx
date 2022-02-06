8d0abc4676e6ca3ff2f2335da5db5de3a600b5dd
cohlerCoded <jeffcohler@cohlercoded.com>
2022-02-06 14:39:53 -0800

    start of textInput animation

--- easy-shop/Screens/Admin/AnimationSandbox.jsx
+++ easy-shop/Screens/Admin/AnimationSandbox.jsx
@@ -1,40 +1,57 @@
-import { StyleSheet, Text, View, Animated } from 'react-native'
+import { StyleSheet, Text, View, Animated, TextInput } from 'react-native'
 import React, { useState, useEffect, useRef } from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const AnimationSandbox = () => {
   const translation = useRef(new Animated.Value(0)).current
+  const translationPlaceHolder = useRef(new Animated.Value(0)).current
   let value = 0
-  const moveSquare = () => {
-    Animated.timing(translation, {
-      toValue: value + 50,
+  //   const moveSquare = () => {
+  //     Animated.timing(translation, {
+  //       toValue: value + 50,
+  //       useNativeDriver: true,
+  //     }).start()
+  //     value += 50
+  //   }
+  const movePlaceHolder = () => {
+    Animated.timing(translationPlaceHolder, {
+      toValue: -25,
       useNativeDriver: true,
     }).start()
-    value += 50
   }
-  useEffect(() => {
-    moveSquare()
-  }, [moveSquare])
+  const movePlaceHolderBack = () => {
+    Animated.timing(translationPlaceHolder, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start()
+  }
+  //   useEffect(() => {
+  //     movePlaceHolder()
+  //   }, [movePlaceHolder])
 
   return (
     <Animated.View
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: 'orange',
-        transform: [{ translateX: translation }],
-      }}
+    //   style={{
+    //     width: 100,
+    //     height: 100,
+    //     backgroundColor: 'orange',
+    //     transform: [{ translateX: translation }],
+    //   }}
     >
-      <TouchableOpacity
+      <TextInput
+        style={{ margin: 20, borderWidth: 1 }}
+        onFocus={movePlaceHolder}
+        onBlur={movePlaceHolderBack}
+      />
+      <Animated.View
         style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'orange',
+          position: 'absolute',
+          margin: 20,
+          transform: [{ translateY: translationPlaceHolder }],
         }}
-        onPress={moveSquare}
       >
-        <Text>AnimationSandbox</Text>
-      </TouchableOpacity>
+        <Text style={{ marginLeft: 5, marginTop: 5 }}>PlaceHolder</Text>
+      </Animated.View>
     </Animated.View>
   )
 }
