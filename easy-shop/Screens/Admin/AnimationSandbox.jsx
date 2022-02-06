6ae848d204113fabec9425263fac1677fa66c2c0
cohlerCoded <jeffcohler@cohlercoded.com>
2022-02-06 13:44:11 -0800

    using state and useEffect to animate

--- easy-shop/Screens/Admin/AnimationSandbox.jsx
+++ easy-shop/Screens/Admin/AnimationSandbox.jsx
@@ -1,8 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AnimationSandbox = () => {
-  const [translation, setTranslation] = useState(50)
+  const [translation, setTranslation] = useState(0)
+  useEffect(() => {
+    for (let i = 0; i < 100; i++) {
+      setTimeout(() => {
+        setTranslation(i)
+      }, 25 * i)
+    }
+  }, [])
 
   return (
     <View style={styles.square} transform={[{ translateX: translation }]}>
