722595195d3a7bbdcf08eece81b0a6968820cfb8
cohlerCoded <jeffcohler@cohlercoded.com>
2022-02-17 22:42:05 -0800

    attempt to optimize list

--- easy-shop/Screens/Cart/Checkout/Checkout.jsx
+++ easy-shop/Screens/Cart/Checkout/Checkout.jsx
@@ -1,20 +1,24 @@
 import { HStack, VStack, Select, CheckIcon } from 'native-base'
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useMemo } from 'react'
 import {
+  FlatList,
   Button,
   StyleSheet,
   Text,
   View,
   Dimensions,
   TouchableOpacity,
+  Image,
 } from 'react-native'
 import { useSelector } from 'react-redux'
 import AnimatedInput from '../../../Components/AnimatedInput'
 import FormContainer from '../../../Components/FormContainer'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import countries from 'world_countries_lists/data/countries/en/countries.json'
+import flags from 'world_countries_lists/data/flags/24x24/flags-24x24.json'
 
 const { width } = Dimensions.get('window')
-
+const countryData = countries.concat(flags)
 const states = [
   'AL - Alabama',
   'AK - Alaska',
@@ -82,6 +86,7 @@ const Checkout = ({ navigation }) => {
   const [address1, setAddress1] = useState('')
   const [address2, setAddress2] = useState('')
   const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
   const [zip, setZip] = useState('')
   useEffect(() => {
     setState('')
@@ -91,17 +96,9 @@ const Checkout = ({ navigation }) => {
       setFirstName('')
     }
   }, [])
-  console.log(width)
   return (
-    <FormContainer>
-      <Text
-        style={{
-          fontSize: 30,
-          textAlign: 'center',
-        }}
-      >
-        &#x1F4E6; Checkout &#x1F4E6;
-      </Text>
+    <FormContainer title='&#x1F4E6; Checkout &#x1F4E6;'>
+      <Text>{`${countries[0].name}`}</Text>
       <KeyboardAwareScrollView
         enableOnAndroid={true}
         viewIsInsideTabBar={true}
@@ -175,7 +172,7 @@ const Checkout = ({ navigation }) => {
           </HStack>
           <HStack width='25%'>
             <Select
-              onOpen={() => setTimeout(() => setSelectFocus(true), 170)}
+              onOpen={() => setTimeout(() => setSelectFocus(true), 180)}
               onClose={() => setSelectFocus(false)}
               marginLeft={-0.5}
               width='80%'
@@ -214,6 +211,72 @@ const Checkout = ({ navigation }) => {
               placeHolder={'Zip'}
               value={zip}
               onChangeText={(text) => setZip(text)}
+            />
+          </HStack>
+        </VStack>
+        <VStack
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginHorizontal: 5,
+            width: '100%',
+          }}
+        >
+          <HStack width='50%'>
+            <Select
+              onOpen={() => setTimeout(() => setSelectFocus(true), 400)}
+              onClose={() => setSelectFocus(false)}
+              marginLeft={-0.5}
+              width='50%'
+              borderColor='#0369a1'
+              borderRadius={0}
+              borderWidth={2}
+              height={10}
+              selectedValue={country}
+              accessibilityLabel='Country'
+              placeholderTextColor='rgb(125, 211, 252)'
+              fontSize='16'
+              placeholder='Country'
+              _selectedItem={{
+                bg: 'teal.600',
+                endIcon: <CheckIcon size='5' />,
+              }}
+              onValueChange={(itemValue) => {
+                setCountry(itemValue)
+                setSelectFocus(false)
+              }}
+            >
+              {useMemo(
+                () =>
+                  countries.map((country, i) => (
+                    <Select.Item
+                      key={i}
+                      leftIcon={
+                        <Image
+                          source={{ uri: flags[country.alpha2] }}
+                          style={{ width: 24, height: 24 }}
+                        />
+                      }
+                      label={
+                        selectFocus === true
+                          ? `${country.alpha2.toUpperCase()} - ${country.name}`
+                          : country.alpha2.toUpperCase()
+                      }
+                      value={country.alpha2.toUpperCase()}
+                    />
+                  )),
+                [{ countries }, { flags }]
+              )}
+            </Select>
+          </HStack>
+          <HStack width='50%'>
+            <AnimatedInput
+              width='46%'
+              fontSize={16}
+              borderWidth={2}
+              placeHolder={'Last Name'}
+              value={lastName}
+              onChangeText={(text) => setLastName(text)}
             />
           </HStack>
         </VStack>
