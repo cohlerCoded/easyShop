4138936d9ccd98f2a3bde08ac82d036fc2242854
cohlerCoded <jeffcohler@cohlercoded.com>
2022-03-25 15:17:17 -0700

    payment setup

--- easy-shop/Screens/Cart/Checkout/Payment.jsx
+++ easy-shop/Screens/Cart/Checkout/Payment.jsx
@@ -1,19 +1,51 @@
+import { Container, VStack, Heading, Radio, HStack } from 'native-base'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-const Payment = () => {
+const methods = [
+  { name: 'Cash on Delivery', value: 1 },
+  { name: 'Bank Transfer', value: 2 },
+  { name: 'Card Payment', value: 3 },
+]
+
+const Payment = (props) => {
+  const order = props.route.params
+  const [selected, setSelected] = useState()
+  const [card, setCard] = useState()
   return (
-    <View>
-      <Text
-        style={{
-          marginTop: 30,
-          fontSize: 30,
-          textAlign: 'center',
-        }}
-      >
-        &#x1F4B8; Payment &#x1F4B8;
-      </Text>
-    </View>
+    <Container>
+      <Heading>
+        <View>
+          <Text
+            style={{
+              marginTop: 30,
+              fontSize: 30,
+              textAlign: 'center',
+            }}
+          >
+            &#x1F4B8; Payment &#x1F4B8;
+          </Text>
+        </View>
+      </Heading>
+
+      <VStack>
+        {methods.map((item, index) => {
+          return (
+            <Radio.Group
+              key={item.name}
+              onPress={() => setSelected(item.value)}
+            >
+              <HStack>
+                <Text>{item.name}</Text>
+              </HStack>
+              <HStack>
+                <Radio selected={selected == item.value} />
+              </HStack>
+            </Radio.Group>
+          )
+        })}
+      </VStack>
+    </Container>
   )
 }
 
